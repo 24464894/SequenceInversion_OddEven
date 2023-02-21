5145df10ce7a0d033b6f38f9f53efebb7e08852b
Alex <smpss96219@gmail.com>
2023-02-21 13:24:28 +0800

    加入專案檔案。

---
+++ Odd_Even/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odd_Even
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("輸入序列:");
+            string input = Console.ReadLine();
+            string[] text = input.Split(',');
+            int[] IntText = Array.ConvertAll(text, s => int.Parse(s));
+            var EvenNumber = IntText.Where(x => x%2 == 0).ToList();
+            Console.Write("奇數 :");
+            foreach (var item in EvenNumber)
+            {
+                Console.Write(item);
+                Console.Write(",");
+            }
+            var OddNumber = IntText.Where(x => x % 2 != 0).ToList();
+            Console.Write("偶數:");
+            foreach (var item in OddNumber)
+            {
+                Console.Write(item);
+                Console.Write(",");
+            }
+        }
+    }
+}
---
+++ SequenceInversion/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SequenceInversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("輸入序列:");
+            string input = Console.ReadLine();
+            string[] text = input.Split(',');
+
+            var number = text.OrderByDescending(x => x).ToList();
+            Console.Write("結果序列");
+            foreach (var item in number)
+            {
+                Console.Write(item);
+                Console.Write(",");
+            }
+            Console.ReadLine();
+        }
+      
+        
+    }
+}
